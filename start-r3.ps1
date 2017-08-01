a344fc80f64632c990725d3420ad7499105dbac4
Mike Baker <michael.baker@apexdatasolutions.net>
2017-08-01 18:24:55 -0400

    Added Tasks class and fixed up scripts to build mount points and db directories to check for existence.

--- start-r3.ps1
+++ start-r3.ps1
@@ -27,13 +27,20 @@ $intAnswer1 = $a.popup("Do you want to extract Globals?", ` 
 0,"Extracting Globals",4) 
 If ($intAnswer1 -eq 6) { 
     Expand-ZIPFile –File “$dir\VistaGlobals.zip” –Destination "$dir\client\globals"
+} 
+
+$a = new-object -comobject wscript.shell 
+$intAnswer2 = $a.popup("Do you want to extract Routines?", ` 
+0,"Extracting Globals",4) 
+If ($intAnswer2 -eq 6) { 
     Expand-ZIPFile –File “$dir\VistaRoutines.zip” –Destination "$dir\client\routines"
 } 
 
 $a = new-object -comobject wscript.shell 
-$intAnswer2 = $a.popup("Do you want to extract Databases?", ` 
+$intAnswer3 = $a.popup("Do you want to extract Databases?", ` 
 0,"Extracting Globals",4) 
-If (($intAnswer2 -eq 6)-and($intAnswer1 -ne 6)) { 
+#If (($intAnswer2 -eq 6)-and($intAnswer1 -ne 6)) {
+ If ($intAnswer3 -eq 6) {
     Expand-ZIPFile –File “$dir\databases.zip” –Destination "$dir"
 } 
 
@@ -59,10 +66,10 @@ While (1 -eq 1) {
     Start-Sleep -s 1
 }
 
-If ($intAnswer2 -ne 6) { 
+#If ($intAnswer3 -ne 6) { 
 	docker exec -it rdw /scripts/mountPoints.sh
 	docker exec -it rdw /scripts/dbDirectory.sh
-}
+#}
 
 Start-Sleep -s 10
 docker-compose up -d cachev08
